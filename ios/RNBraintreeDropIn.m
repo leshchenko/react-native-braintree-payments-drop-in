ec9da227f16da98de5fd8aee6e1a6cc1b711b946
Beka Lomadze <beka.m.lomadze@gmail.com>
2019-10-03 17:56:41 +0300

    apple pay support added

--- ios/RNBraintreeDropIn.m
+++ ios/RNBraintreeDropIn.m
@@ -1,4 +1,14 @@
 #import "RNBraintreeDropIn.h"
+@import PassKit;
+#import "BraintreeApplePay.h"
+
+@interface RNBraintreeDropIn() <PKPaymentAuthorizationViewControllerDelegate>
+
+@property (nonatomic, strong) BTAPIClient *apiClient;
+@property (nonatomic, strong) RCTPromiseResolveBlock resolve;
+@property (nonatomic, strong) RCTPromiseRejectBlock reject;
+
+@end
 
 @implementation RNBraintreeDropIn
 
@@ -48,6 +58,23 @@ RCT_REMAP_METHOD(show,
                     } else {
                         [[self class] resolvePayment :result resolver:resolve];
                     }
+                } else if (result.paymentOptionType == BTUIKPaymentOptionTypeApplePay) {
+                    if (!options[@"companyName"]) {
+                        reject(@"NO_COMPANY_NAME", @"You must provide a company name", nil);
+                        return;
+                    }
+                    if (!options[@"totalPrice"]) {
+                        reject(@"NO_TOTAL_PRICE", @"You must provide a total price", nil);
+                        return;
+                    }
+                    self.apiClient = [[BTAPIClient alloc] initWithAuthorization:clientToken];
+                    self.resolve = resolve;
+                    self.reject = reject;
+                    [self setupPaymentRequest:^(PKPaymentRequest *paymentRequest, NSError *error) {
+                        PKPaymentAuthorizationViewController *vc = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:paymentRequest];
+                        vc.delegate = self;
+                        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:vc animated:YES completion:NULL];
+                    } options: options];
                 } else {
                     [[self class] resolvePayment :result resolver:resolve];
                 }
@@ -70,6 +97,39 @@ RCT_REMAP_METHOD(show,
     resolve(jsResult);
 }
 
+- (void)setupPaymentRequest:(void (^)(PKPaymentRequest * _Nullable, NSError * _Nullable))completion options:(NSDictionary*)options {
+    BTApplePayClient *applePayClient = [[BTApplePayClient alloc] initWithAPIClient:self.apiClient];
+    // You can use the following helper method to create a PKPaymentRequest which will set the `countryCode`,
+    // `currencyCode`, `merchantIdentifier`, and `supportedNetworks` properties.
+    // You can also create the PKPaymentRequest manually. Be aware that you'll need to keep these in
+    // sync with the gateway settings if you go this route.
+    [applePayClient paymentRequest:^(PKPaymentRequest * _Nullable paymentRequest, NSError * _Nullable error) {
+        if (error) {
+            completion(nil, error);
+            return;
+        }
+
+        // We recommend collecting billing address information, at minimum
+        // billing postal code, and passing that billing postal code with all
+        // Apple Pay transactions as a best practice.
+//        if (@available(iOS 11.0, *)) {
+//            paymentRequest.requiredBillingContactFields = [NSSet setWithObject:PKContactFieldPostalAddress];
+//        }
+
+        // Set other PKPaymentRequest properties here
+        paymentRequest.merchantCapabilities = PKMerchantCapability3DS;
+        paymentRequest.paymentSummaryItems =
+        @[
+            //[PKPaymentSummaryItem summaryItemWithLabel:options[@"itemName"] amount:[NSDecimalNumber decimalNumberWithString:options[@"totalPrice"]]],
+            // Add add'l payment summary items...
+            [PKPaymentSummaryItem summaryItemWithLabel:options[@"companyName"] amount:[NSDecimalNumber decimalNumberWithString:[NSString stringWithFormat:@"%@", options[@"totalPrice"]]]]
+        ];
+
+        // Save the PKPaymentRequest or start the payment flow
+        completion(paymentRequest, nil);
+    }];
+}
+
 - (UIViewController*)reactRoot {
     UIViewController *root  = [UIApplication sharedApplication].keyWindow.rootViewController;
     UIViewController *maybeModal = root.presentedViewController;
@@ -81,6 +141,49 @@ RCT_REMAP_METHOD(show,
     }
 
     return modalRoot;
+}
+
+- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
+                       didAuthorizePayment:(PKPayment *)payment
+                                completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+
+    // Example: Tokenize the Apple Pay payment
+    BTApplePayClient *applePayClient = [[BTApplePayClient alloc]
+                                        initWithAPIClient:self.apiClient];
+    [applePayClient tokenizeApplePayPayment:payment
+                                 completion:^(BTApplePayCardNonce *tokenizedApplePayPayment,
+                                              NSError *error) {
+        if (tokenizedApplePayPayment) {
+            // On success, send nonce to your server for processing.
+            NSLog(@"nonce = %@", tokenizedApplePayPayment.nonce);
+
+            // If requested, address information is accessible in `payment` and may
+            // also be sent to your server.
+//            NSLog(@"billingPostalCode = %@", payment.billingContact.postalAddress.postalCode);
+
+            // Then indicate success or failure via the completion callback, e.g.
+            NSMutableDictionary* jsResult = [NSMutableDictionary new];
+            [jsResult setObject:tokenizedApplePayPayment.nonce forKey:@"nonce"];
+            self.resolve(jsResult);
+            completion(PKPaymentAuthorizationStatusSuccess);
+        } else {
+            // Tokenization failed. Check `error` for the cause of the failure.
+            self.reject(error.localizedDescription, error.localizedDescription, error);
+            // Indicate failure via the completion callback:
+            completion(PKPaymentAuthorizationStatusFailure);
+        }
+        self.resolve = NULL;
+        self.reject = NULL;
+    }];
+}
+
+- (void)paymentAuthorizationViewControllerDidFinish:(nonnull PKPaymentAuthorizationViewController *)controller {
+    [self.reactRoot dismissViewControllerAnimated:YES completion:NULL];
+    if (self.reject) {
+        self.reject(@"USER_CANCELLATION", @"The user cancelled", nil);
+    }
+    self.resolve = NULL;
+    self.reject = NULL;
 }
 
 @end
