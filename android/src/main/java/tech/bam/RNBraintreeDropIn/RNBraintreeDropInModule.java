8022d11c2192f43db935fa35e533afd1c09cdcda
Beka Lomadze <beka.m.lomadze@gmail.com>
2019-12-09 16:26:04 +0200

    changed java module a bit

--- android/src/main/java/tech/bam/RNBraintreeDropIn/RNBraintreeDropInModule.java
+++ android/src/main/java/tech/bam/RNBraintreeDropIn/RNBraintreeDropInModule.java
@@ -3,16 +3,19 @@ package tech.bam.RNBraintreeDropIn;
 import android.app.Activity;
 import android.content.Intent;
 
+import androidx.annotation.NonNull;
+
 import com.braintreepayments.api.models.GooglePaymentRequest;
+import com.braintreepayments.api.models.ThreeDSecureAdditionalInformation;
+import com.braintreepayments.api.models.ThreeDSecurePostalAddress;
+import com.braintreepayments.api.models.ThreeDSecureRequest;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
-import com.facebook.react.bridge.Promise;
 import com.braintreepayments.api.dropin.DropInActivity;
 import com.braintreepayments.api.dropin.DropInRequest;
 import com.braintreepayments.api.dropin.DropInResult;
@@ -21,131 +24,154 @@ import com.braintreepayments.api.models.CardNonce;
 import com.braintreepayments.api.models.ThreeDSecureInfo;
 import com.google.android.gms.wallet.TransactionInfo;
 import com.google.android.gms.wallet.WalletConstants;
+import com.facebook.react.bridge.Promise;
 
 
 public class RNBraintreeDropInModule extends ReactContextBaseJavaModule {
 
-  private Promise mPromise;
-  private static final int DROP_IN_REQUEST = 0x444;
+    private Promise mPromise;
+    private static final int DROP_IN_REQUEST = 0x444;
 
-  private boolean isVerifyingThreeDSecure = false;
+    RNBraintreeDropInModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        reactContext.addActivityEventListener(new BaseActivityEventListener() {
+            @Override
+            public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+                super.onActivityResult(activity, requestCode, resultCode, data);
 
-  public RNBraintreeDropInModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    reactContext.addActivityEventListener(mActivityListener);
-  }
+                if (requestCode != DROP_IN_REQUEST || mPromise == null) {
+                    return;
+                }
 
-  @ReactMethod
-  public void show(final ReadableMap options, final Promise promise) {
-    isVerifyingThreeDSecure = false;
+                if (resultCode == Activity.RESULT_OK) {
+                    DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                    PaymentMethodNonce paymentMethodNonce = result.getPaymentMethodNonce();
 
-    if (!options.hasKey("clientToken")) {
-      promise.reject("NO_CLIENT_TOKEN", "You must provide a client token");
-      return;
+                    if (paymentMethodNonce instanceof CardNonce) {
+                        CardNonce cardNonce = (CardNonce) paymentMethodNonce;
+                        ThreeDSecureInfo threeDSecureInfo = cardNonce.getThreeDSecureInfo();
+                        if (!threeDSecureInfo.isLiabilityShiftPossible()) {
+                            mPromise.reject("3DSECURE_NOT_ABLE_TO_SHIFT_LIABILITY", "3D Secure liability cannot be shifted");
+                        } else if (!threeDSecureInfo.isLiabilityShifted()) {
+                            mPromise.reject("3DSECURE_LIABILITY_NOT_SHIFTED", "3D Secure liability was not shifted");
+                        } else {
+                            resolvePayment(paymentMethodNonce);
+                        }
+                    }
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    mPromise.reject("USER_CANCELLATION", "The user cancelled");
+                } else {
+                    Exception exception = (Exception) data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+                    mPromise.reject(exception.getMessage(), exception.getMessage());
+                }
+
+                mPromise = null;
+            }
+        });
     }
 
-    Activity currentActivity = getCurrentActivity();
-    if (currentActivity == null) {
-      promise.reject("NO_ACTIVITY", "There is no current activity");
-      return;
-    }
-
-    boolean disabledVaultManager = !options.hasKey("disabledVaultManager")
-            || (options.hasKey("disabledVaultManager")
-            && !options.getBoolean("disabledVaultManager"));
-
-    DropInRequest dropInRequest = new DropInRequest()
-            .vaultManager(disabledVaultManager)
-            .clientToken(options.getString("clientToken"));
-
-    enableGooglePay(dropInRequest, options);
-
-    if (options.hasKey("threeDSecure")) {
-      final ReadableMap threeDSecureOptions = options.getMap("threeDSecure");
-      if (!threeDSecureOptions.hasKey("amount")) {
-        promise.reject("NO_3DS_AMOUNT", "You must provide an amount for 3D Secure");
-        return;
-      }
-
-      isVerifyingThreeDSecure = true;
-
-      dropInRequest
-              .amount(String.valueOf(threeDSecureOptions.getDouble("amount")))
-              .requestThreeDSecureVerification(true);
-    }
-
-    mPromise = promise;
-    currentActivity.startActivityForResult(dropInRequest.getIntent(currentActivity), DROP_IN_REQUEST);
-  }
-
-  private void enableGooglePay(DropInRequest dropInRequest, ReadableMap options) {
-    String totalPrice = Double.toString(options.getDouble("totalPrice"));
-    String currencyCode = options.getString("currencyCode");
-    String merchantId = options.getString("GPayMerchantId");
-    String env = "test".equals(merchantId) ? "TEST" : "PRODUCTION";
-    if(totalPrice != null && currencyCode != null && merchantId != null) {
-      GooglePaymentRequest googlePaymentRequest = new GooglePaymentRequest()
-              .transactionInfo(TransactionInfo.newBuilder()
-                      .setTotalPrice(totalPrice)
-                      .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
-                      .setCurrencyCode(currencyCode)
-                      .build())
-              .billingAddressRequired(true)
-              .googleMerchantId(merchantId)
-              .environment(env);
-      dropInRequest.googlePaymentRequest(googlePaymentRequest);
-    }
-  }
-
-  private final ActivityEventListener mActivityListener = new BaseActivityEventListener() {
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-      super.onActivityResult(requestCode, resultCode, data);
-
-      if (requestCode != DROP_IN_REQUEST || mPromise == null) {
-        return;
-      }
-
-      if (resultCode == Activity.RESULT_OK) {
-        DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
-        PaymentMethodNonce paymentMethodNonce = result.getPaymentMethodNonce();
-
-        if (isVerifyingThreeDSecure && paymentMethodNonce instanceof CardNonce) {
-          CardNonce cardNonce = (CardNonce) paymentMethodNonce;
-          ThreeDSecureInfo threeDSecureInfo = cardNonce.getThreeDSecureInfo();
-          if (!threeDSecureInfo.isLiabilityShiftPossible()) {
-            mPromise.reject("3DSECURE_NOT_ABLE_TO_SHIFT_LIABILITY", "3D Secure liability cannot be shifted");
-          } else if (!threeDSecureInfo.isLiabilityShifted()) {
-            mPromise.reject("3DSECURE_LIABILITY_NOT_SHIFTED", "3D Secure liability was not shifted");
-          } else {
-            resolvePayment(paymentMethodNonce);
-          }
-        } else {
-          resolvePayment(paymentMethodNonce);
+    @ReactMethod
+    public void show(final ReadableMap options, final Promise promise) {
+        if (!options.hasKey("clientToken")) {
+            promise.reject("NO_CLIENT_TOKEN", "You must provide a client token");
+            return;
         }
-      } else if (resultCode == Activity.RESULT_CANCELED) {
-        mPromise.reject("USER_CANCELLATION", "The user cancelled");
-      } else {
-        Exception exception = (Exception) data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
-        mPromise.reject(exception.getMessage(), exception.getMessage());
-      }
 
-      mPromise = null;
+        Activity currentActivity = getCurrentActivity();
+        if (currentActivity == null) {
+            promise.reject("NO_ACTIVITY", "There is no current activity");
+            return;
+        }
+
+        boolean disabledVaultManager = !options.hasKey("disabledVaultManager")
+                || (options.hasKey("disabledVaultManager")
+                && !options.getBoolean("disabledVaultManager"));
+
+        DropInRequest dropInRequest = new DropInRequest()
+                .vaultManager(disabledVaultManager)
+                .clientToken(options.getString("clientToken"));
+
+        final ReadableMap threeDSecureOptions = options.getMap("threeDSecure");
+        if (threeDSecureOptions == null) {
+            promise.reject("THREEDSECURE_IS_NULL", "3D Secure options were not provided");
+            return;
+        }
+
+        final ThreeDSecurePostalAddress address;
+        try {
+            address = new ThreeDSecurePostalAddress()
+                    .givenName(threeDSecureOptions.getString("firstName"))
+                    .surname(threeDSecureOptions.getString("lastName"))
+                    .phoneNumber(threeDSecureOptions.getString("phoneNumber"))
+                    .streetAddress(threeDSecureOptions.getString("streetAddress"))
+                    .extendedAddress(threeDSecureOptions.getString("streetAddress2"))
+                    .locality(threeDSecureOptions.getString("city"))
+                    .region(threeDSecureOptions.getString("region"))
+                    .postalCode(threeDSecureOptions.getString("postalCode"))
+                    .countryCodeAlpha2(threeDSecureOptions.getString("countryCode"));
+        } catch (Exception error) {
+            promise.reject("ADDRESS_ERROR", "Failed to prepare address");
+            return;
+        }
+
+        ThreeDSecureAdditionalInformation additionalInformation = new ThreeDSecureAdditionalInformation()
+                .shippingAddress(address);
+
+        ThreeDSecureRequest threeDSecureRequest;
+        try {
+            threeDSecureRequest = new ThreeDSecureRequest()
+                    .amount(threeDSecureOptions.getString("amount"))
+                    .email(threeDSecureOptions.getString("email"))
+                    .billingAddress(address)
+                    .versionRequested(ThreeDSecureRequest.VERSION_2)
+                    .additionalInformation(additionalInformation);
+        } catch (Exception error) {
+            promise.reject("THREEDSECURE_FAILED", error.getMessage());
+            return;
+        }
+
+        try {
+            String amount = threeDSecureOptions.getString("amount");
+            String currencyCode = options.getString("currencyCode");
+            String merchantId = options.getString("GPayMerchantId");
+            String env = "test".equals(merchantId) ? "TEST" : "PRODUCTION";
+            if (merchantId != null && amount != null && currencyCode != null) {
+                GooglePaymentRequest googlePaymentRequest = new GooglePaymentRequest()
+                        .transactionInfo(TransactionInfo.newBuilder()
+                                .setTotalPrice(amount)
+                                .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                                .setCurrencyCode(currencyCode)
+                                .build())
+                        .billingAddressRequired(true)
+                        .googleMerchantId(merchantId)
+                        .environment(env);
+                dropInRequest.googlePaymentRequest(googlePaymentRequest);
+            }
+        } catch (Exception ignored) {
+        }
+
+        dropInRequest
+                .requestThreeDSecureVerification(true)
+                .threeDSecureRequest(threeDSecureRequest);
+
+        mPromise = promise;
+        currentActivity.startActivityForResult(dropInRequest.getIntent(currentActivity), DROP_IN_REQUEST);
     }
-  };
 
-  private final void resolvePayment(PaymentMethodNonce paymentMethodNonce) {
-    WritableMap jsResult = Arguments.createMap();
-    jsResult.putString("nonce", paymentMethodNonce.getNonce());
-    jsResult.putString("type", paymentMethodNonce.getTypeLabel());
-    jsResult.putString("description", paymentMethodNonce.getDescription());
-    jsResult.putBoolean("isDefault", paymentMethodNonce.isDefault());
 
-    mPromise.resolve(jsResult);
-  }
+    private void resolvePayment(PaymentMethodNonce paymentMethodNonce) {
+        WritableMap jsResult = Arguments.createMap();
+        jsResult.putString("nonce", paymentMethodNonce.getNonce());
+        jsResult.putString("type", paymentMethodNonce.getTypeLabel());
+        jsResult.putString("description", paymentMethodNonce.getDescription());
+        jsResult.putBoolean("isDefault", paymentMethodNonce.isDefault());
 
-  @Override
-  public String getName() {
-    return "RNBraintreeDropIn";
-  }
+        mPromise.resolve(jsResult);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "RNBraintreeDropIn";
+    }
 }
